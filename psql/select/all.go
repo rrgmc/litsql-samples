3d6b16988b5ab05fc35afe2c8e21280e4f7a4b7f
Rangel Reale <rangelspam@gmail.com>
2024-06-01 07:03:29 -0300

    sample all

---
+++ psql/select/all.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"github.com/rrgmc/litsql"
+	"github.com/rrgmc/litsql-samples/util"
+	"github.com/rrgmc/litsql/dialect/psql"
+	"github.com/rrgmc/litsql/dialect/psql/sm"
+	"github.com/rrgmc/litsql/expr"
+)
+
+func all() error {
+	query := psql.Select(
+		sm.With("c", "id", "data").
+			As(psql.Select(
+				sm.Columns("id"),
+				sm.From("test1"),
+				sm.LeftJoin("test2").Using("hhh", "jjjj"),
+			)),
+		sm.With("h", "id2", "data2").As(psql.Select(
+			sm.Columns("id2"),
+			sm.From("test12"),
+		)),
+		sm.Distinct("b", "c"),
+		sm.Columns("a", "b", "c"),
+		sm.Columns("d", "e", "f"),
+		sm.FromQ(psql.Select(
+			sm.From("xt"),
+			sm.Where("x = 12"),
+			sm.WhereC("h = ?", litsql.ArgDefault("x", 9122)),
+			// sm.WhereC("j = ?", sql.Named("x", 444)),
+			// sm.WhereC("j = ?", sqlb.DBArg("x")),
+		)),
+		sm.InnerJoin("device").As("x").On("d.x = d.y").On("abc = def"),
+		sm.InnerJoin("double").As("h").On("h.j = x.t"),
+		sm.Where("j = 5 AND k = 12"),
+		sm.WhereC("j IN ?", expr.InP(litsql.Arg("x"), 2, 3)),
+		sm.WhereC("h IN ?", psql.Select(
+			sm.From("xxx"),
+		)),
+		sm.WhereC("j = ? AND k = ?", litsql.ArgFunc(func() (any, error) {
+			return "99", nil
+		}), litsql.Arg("second")),
+		sm.WhereE(
+			expr.Or(
+				"a = 5 AND b = 12",
+				"t = 5 AND s = 12",
+			),
+		),
+		sm.WhereE(
+			expr.OrE(
+				expr.Paren("a = 5 AND b = 12"),
+				expr.Paren("t = 5 AND s = 12"),
+			),
+		),
+		sm.Func(func(a psql.SelectModApply) {
+			a.Apply(
+				sm.Columns("ii", "bb"),
+				sm.Where("x = 15"),
+			)
+		}),
+		sm.Window("abc").PartitionBy(expr.Raw("depname")).OrderBy(expr.Raw("salary")).From("uuu").Groups(),
+		sm.Window("xyz").PartitionBy(expr.Raw("tutor")).OrderBy(expr.Raw("body")),
+		sm.GroupBy("a", "b").Distinct(),
+		sm.Having("b > 12"),
+		sm.OrderBy("b DESC", "c"),
+		sm.OffsetA(10),
+		sm.LimitA(99),
+		sm.Union(psql.Select(
+			sm.Columns("a", "b", "c"),
+			sm.From("ttt111"),
+		)),
+		sm.Union(psql.Select(
+			sm.Columns("t", "a", "x"),
+			sm.From("uuuu8888"),
+		)),
+	)
+	return util.PrintQuery(query, map[string]any{
+		"x":      56,
+		"second": 2,
+	})
+}
--- psql/select/main.go
+++ psql/select/main.go
@@ -1,7 +1,7 @@
 package main
 
 func main() {
-	err := simple()
+	err := all()
 	if err != nil {
 		panic(err)
 	}
