24cdd9207f352f9753a8451444540198e284750f
Rangel Reale <rangelspam@gmail.com>
2024-06-07 09:33:06 -0300

    README

--- psql/select/sample.go
+++ psql/select/sample.go
@@ -3,24 +3,10 @@ package main
 import (
 	"github.com/rrgmc/litsql-samples/util"
 	"github.com/rrgmc/litsql/dialect/psql"
-	"github.com/rrgmc/litsql/dialect/psql/sm"
+	"github.com/rrgmc/litsql/expr"
 )
 
 func sample() error {
-	type userFilter struct {
-		Name string
-	}
-	filter := userFilter{
-		Name: "john",
-	}
-	query := psql.Select(
-		sm.Columns("id", "name"),
-		sm.From("users"),
-	)
-	if filter.Name != "" {
-		query.Apply(
-			sm.WhereClause("name = ?", filter.Name),
-		)
-	}
+	query := psql.SelectRawExpr(expr.Clause("select * from users where user_id = ?", 55))
 	return util.PrintQuery(query, nil)
 }
